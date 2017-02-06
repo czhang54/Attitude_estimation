0b0a6fc51c6a88e4fccd8155fddc57eb1c732251
czhang54 <zhangchi314@gmail.com>
2017-02-05 22:25:41 -0600

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -68,7 +68,7 @@ int main()
 	world.add_sensor(&accelerometer);
 	accelerometer.initialize();
 
-	// Magnetometer: ma=easures local megnetic field vector in sensor frame
+	// Magnetometer: measures local megnetic field vector in sensor frame
 	const Vector3d magnetic_field(1,0,1); // Magnetic field vector
 	Magnetometer magnetometer(dim_sensor, sensor_noise_std, magnetic_field);
 	world.add_sensor(&magnetometer);
@@ -78,25 +78,25 @@ int main()
 
 	world.simulate_sensor(generator); // Simulate sensors and obtain measurements
 
-	/* Build MEKF */
+	/* Build Multiplicative EKF (MEKF) */
 	MEKF *mekf = new MEKF(IC_filter, IC_std);
 	world.add_filter(mekf);
 	mekf->initialize(generator);
 	std::cout << mekf;
 
-	/* Build IEKF */
+	/* Build Invariant EKF (IEKF) */
 	IEKF iekf(IC_filter, IC_std);
 	world.add_filter(&iekf);
 	iekf.initialize(generator);
 	std::cout << &iekf;
 
-	/* Build particle filter */
+	/* Build Particle Filter (PF) */
 	ParticleFilter pf(IC_filter, IC_std, num_particles, 0.1);
 	world.add_filter(&pf);
 	pf.initialize(generator);
 	std::cout << &pf;
 
-	/* Build FPF */
+	/* Build Feedback Particle Filter (FPF) */
 	FPF *fpf = new FPF(IC_filter, IC_std, num_particles, "galerkin", 10, 50);
 	world.add_filter(fpf);
 	fpf->initialize(generator);
