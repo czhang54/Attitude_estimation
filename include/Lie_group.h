3ea9b6f01897e97b22ab46ae47786beeb4f06816
czhang54 <zhangchi314@gmail.com>
2017-02-05 23:39:21 -0600

    Add files via upload
    Removed "using namespace Eigen" or moved it inside namespace Attitude_estimation.

--- include/Lie_group.h
+++ include/Lie_group.h
@@ -8,7 +8,9 @@
 #include <Eigen/Dense>
 
 // using namespace std;
-using namespace Eigen;
+// using namespace Eigen;
+
+/* This header file declares functions related to computations using quaternions and/or rotation matrices */
 
 typedef Eigen::Vector4d quaternion;
 typedef Eigen::Matrix3d RotationMatrix;
@@ -17,28 +19,28 @@ typedef Eigen::Matrix3d RotationMatrix;
 quaternion quaternion_product(const quaternion &q1, const quaternion &q2);
 
 // Update quaternion according to angular velocity vector defined in body/global frame
-quaternion dq(const quaternion &q, const Vector3d &omega, std::string frame="body_frame");
+quaternion dq(const quaternion &q, const Eigen::Vector3d &omega, std::string frame="body_frame");
 
 // Convert quaternion to rotation matrix
 RotationMatrix q2R(const quaternion &q);
 
 // Compute skew-symmetric matrix from given vector
-Matrix3d skew(const Vector3d &v);
+Eigen::Matrix3d skew(const Eigen::Vector3d &v);
 
 // Create identity matrix of given size
-MatrixXd identity(int d);
+Eigen::MatrixXd identity(int d);
 
 // Convert MRP to quaternion
-quaternion MRP2q(const Vector3d &m);
+quaternion MRP2q(const Eigen::Vector3d &m);
 
 // Compute inverse quaternion
 quaternion inverse_quaternion(const quaternion &q);
 
 // Compute quaternion from angle-axis parameter
-quaternion angle_axis(const Vector3d axis, double angle);
+quaternion angle_axis(const Eigen::Vector3d axis, double angle);
 
 // Compute statistical mean of quaternion samples
-quaternion quaternion_mean(MatrixXd &quaternions);
+quaternion quaternion_mean(Eigen::MatrixXd &quaternions);
 
 
 #endif
