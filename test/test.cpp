412d6662534e39144f27fd720ea4904d9d8af185
Chi Zhang <zhangchi314@gmail.com>
2017-02-18 19:54:47 -0600

    Add files via upload

--- test/test.cpp
+++ test/test.cpp
@@ -5,15 +5,23 @@
 #include <Eigen/Dense>
 
 // Header guard is needed to avoid duplicate include
+
 #include "World.h"
 #include "Target.h"
 #include "Sensor.h"
-#include "Filter.h"
 #include "Lie_group.h" 
 
+// Include filters
+#include "Filter.h"
+#include "MultiplicativeEKF.h"
+#include "InvariantEKF.h"
+#include "ParticleFilter.h"
+#include "FeedbackParticleFilter.h"
+
+// Avoid/Minimize using namespace in the global scope
 // using namespace std;
 // using namespace Eigen;
-using namespace Attitude_estimation;
+using namespace attitude_estimation;
 
 
 int main()
@@ -49,8 +57,8 @@ int main()
 
 
 	/* Simulate target */
-	Sinusoidal sinusoidal(IC_target, IC_std, process_noise_std);
-	Target *target = &sinusoidal;
+	Sinusoidal sinusoidal_target(IC_target, IC_std, process_noise_std);
+	TargetBase *target = &sinusoidal_target;
 
 	world.add_target(target);
 	target->initialize(); // Initialization is done after being added to world
@@ -71,18 +79,18 @@ int main()
 	world.add_sensor(&magnetometer);
 	magnetometer.initialize();
 
-	for (Sensor *s: world.get_sensor()){std::cout << s;}
+	for (SensorBase *s: world.get_sensor()){std::cout << s;}
 
-	world.simulate_sensor(generator); // Simulate sensors and obtain measurements
+	world.simulate_sensors(generator); // Simulate sensors and obtain measurements
 
 	/* Build Multiplicative EKF (MEKF) */
-	MEKF *mekf = new MEKF(IC_filter, IC_std);
+	MultiplicativeEKF *mekf = new MultiplicativeEKF(IC_filter, IC_std);
 	world.add_filter(mekf);
 	mekf->initialize(generator);
 	std::cout << mekf;
 
 	/* Build Invariant EKF (IEKF) */
-	IEKF iekf(IC_filter, IC_std);
+	InvariantEKF iekf(IC_filter, IC_std);
 	world.add_filter(&iekf);
 	iekf.initialize(generator);
 	std::cout << &iekf;
@@ -94,15 +102,22 @@ int main()
 	std::cout << &pf;
 
 	/* Build Feedback Particle Filter (FPF) */
-	FPF *fpf = new FPF(IC_filter, IC_std, num_particles, "galerkin", 10, 50);
+	FeedbackParticleFilter *fpf = new FeedbackParticleFilter(IC_filter, IC_std, num_particles, "galerkin", 10, 50);
 	world.add_filter(fpf);
 	fpf->initialize(generator);
 	std::cout << fpf;
 
-	world.simulate_filter(generator); // Simulate all the filters
+	// Simulate all the filters
+	world.simulate_filters(generator); 
 
 
-	std::cout << "Simulation of attitude estimation finished!" << '\n';
+	std::cout << "Simulation of attitude estimation finished!" << '\n'; 
+
+	// Deleting the filter pointers are actually not necessary here
+	delete mekf;
+	mekf = NULL;
+	delete fpf;
+	fpf = NULL;
 
 	return 0;
 
